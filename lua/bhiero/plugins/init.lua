10f56d0e800bb6214fa519b2b6366e33ce305a11
davblckqueen <ing.dsrr@gmail.com>
2025-04-02 11:43:45 -0500

    colorscheme updated

--- lua/bhiero/plugins/init.lua
+++ lua/bhiero/plugins/init.lua
@@ -7,6 +7,8 @@ return {
     config = function()
       -- load the colorscheme here
       vim.cmd([[colorscheme tokyonight-night]])
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
   }
 }

2a927a79526e29803262af67ed88bf2a1878b292
davblckqueen <ing.dsrr@gmail.com>
2025-04-13 20:36:37 -0500

    lsp remaps and noice plugin added

--- lua/bhiero/plugins/lspconfig.lua
+++ lua/bhiero/plugins/lspconfig.lua
@@ -14,6 +14,29 @@ return {
     config = function()
         local lspconfig = require("lspconfig")
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+        local on_attach = function(client, bufnr)
+            local buf_map = function(mode, lhs, rhs, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, lhs, rhs, opts)
+            end
+
+            -- LSP mappings
+            buf_map('n', 'gd', vim.lsp.buf.definition)           -- Go to definition
+            buf_map('n', 'gD', vim.lsp.buf.declaration)          -- Go to declaration
+            buf_map('n', 'gr', vim.lsp.buf.references)           -- Show references
+            buf_map('n', 'gi', vim.lsp.buf.implementation)       -- Go to implementation
+            buf_map('n', 'K', vim.lsp.buf.hover)                 -- Hover information
+            buf_map('n', '<leader>vd', vim.diagnostic.open_float)
+            buf_map('n', '<leader>vws', vim.lsp.buf.workspace_symbol)
+            buf_map('n', '<C-h>', vim.lsp.buf.signature_help)    -- Signature help
+            buf_map('n', '<leader>rn', vim.lsp.buf.rename)       -- Rename symbol
+            buf_map('n', '<leader>ca', vim.lsp.buf.code_action)  -- Code actions
+            buf_map('n', '[d', vim.diagnostic.goto_prev)         -- Previous diagnostic
+            buf_map('n', ']d', vim.diagnostic.goto_next)         -- Next diagnostic
+            buf_map('n', '<leader>q', vim.diagnostic.setloclist) -- Set location list
+        end
         
         -- Configure clangd for C/C++
         lspconfig.clangd.setup({
--- lua/bhiero/plugins/nvimcmp.lua
+++ lua/bhiero/plugins/nvimcmp.lua
@@ -19,6 +19,29 @@ return {
                 { name = "nvim_lsp" },
                 { name = "buffer" },
                 { name = "path" },
+            }),
+            mapping = cmp.mapping.preset.insert({
+                ['<C-n>'] = cmp.mapping.select_next_item(), -- Navigate to the next item in the completion menu
+                ['<C-p>'] = cmp.mapping.select_prev_item(), -- Navigate to the previous item in the completion menu
+                ['<C-d>'] = cmp.mapping.scroll_docs(-4),    -- Scroll documentation up
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),     -- Scroll documentation down
+                ['<C-Space>'] = cmp.mapping.complete(),     -- Trigger completion
+                ['<C-e>'] = cmp.mapping.abort(),            -- Abort completion
+                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Confirm selection
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item() -- Navigate to the next item if completion menu is visible
+                    else
+                        fallback() -- Fallback to default behavior
+                    end
+                end, { 'i', 's' }),
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item() -- Navigate to the previous item if completion menu is visible
+                    else
+                        fallback() -- Fallback to default behavior
+                    end
+                end, { 'i', 's' }),
             })
         })
     end

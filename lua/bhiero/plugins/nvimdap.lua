b1357453fa7272f75a81312e0e42656e41309cc5
davblckqueen <ing.dsrr@gmail.com>
2025-04-28 05:29:56 -0500

    comment mapping added

--- lua/bhiero/plugins/nvimdap.lua
+++ lua/bhiero/plugins/nvimdap.lua
@@ -1,8 +1,35 @@
 return {
 	"mfussenegger/nvim-dap",
 	config = function()
+		local dap = require("dap")
+		vim.fn.sign_define("DapBreakpoint", {
+			text = "●",
+			texthl = "DapBreakpoint",
+			linehl = "",
+			numhl = "",
+		})
+		vim.cmd([[highlight DapBreakpoint guifg=#FF0000]])
+		vim.fn.sign_define("DapBreakpointCondition", {
+			text = "◆",
+			texthl = "DapBreakpointCondition",
+			linehl = "",
+			numhl = "",
+		})
+		vim.fn.sign_define("DapLogPoint", {
+			text = "◉",
+			texthl = "DapLogPoint",
+			linehl = "",
+			numhl = "",
+		})
+		vim.fn.sign_define("DapStopped", {
+			text = "▶",
+			texthl = "DapStopped",
+			linehl = "DapStoppedLine",
+			numhl = "",
+		})
+
 		vim.keymap.set("n", "<leader>sb", function()
-			vim.cmd("DapToogleBreakpoint <CR>")
-		end)
+			dap.toggle_breakpoint()
+		end, { desc = "Toggle Breakpoint" })
 	end,
 }
